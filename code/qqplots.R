25dc7b272b505d11a0d809b4bc61f69eeed0b383
Dason Kurkiewicz <dasonk@iastate.edu>
2013-05-29 15:20:19 -0500

    Added more to qqplot code

--- code/qqplots.R
+++ code/qqplots.R
@@ -1,6 +1,69 @@
-n <- 30
+n <- 10
+par(mfrow = c(3,3))
+for(i in 1:9){
+  x <- sort(rnorm(n))
+  y <- sort(rnorm(n))
+  qqplot(x, y)
+  abline(lm(y~x))
+}
+layout(1)
+title(paste("qqplots - Both samples from normal. n =", n))
 
-x <- sort(rnorm(n))
-y <- sort(rexp(n))
-qqplot(x, y)
-abline(lm(y~x))
+n <- 30
+par(mfrow = c(3,3))
+for(i in 1:9){
+  x <- sort(rnorm(n))
+  y <- sort(rnorm(n))
+  qqplot(x, y)
+  abline(lm(y~x))
+}
+layout(1)
+title(paste("qqplots - Both samples from normal. n =", n))
+
+n <- 1000
+par(mfrow = c(3,3))
+for(i in 1:9){
+  x <- sort(rnorm(n))
+  y <- sort(rnorm(n))
+  qqplot(x, y)
+  abline(lm(y~x))
+}
+layout(1)
+title(paste("qqplots - Both samples from normal. n =", n))
+
+n <- 10
+par(mfrow = c(3,3))
+for(i in 1:9){
+  x <- sort(rnorm(n))
+  y <- sort(rexp(n))
+  qqplot(x, y)
+  abline(lm(y~x))
+}
+layout(1)
+title(paste("qqplots - Sample 1: normal, Sample 2: exponential. n =", n))
+
+
+n <- 30
+par(mfrow = c(3,3))
+for(i in 1:9){
+  x <- sort(rnorm(n))
+  y <- sort(rexp(n))
+  qqplot(x, y)
+  abline(lm(y~x))
+}
+layout(1)
+title(paste("qqplots - Sample 1: normal, Sample 2: exponential. n =", n))
+
+
+n <- 1000
+par(mfrow = c(3,3))
+for(i in 1:9){
+  x <- sort(rnorm(n))
+  y <- sort(rexp(n))
+  qqplot(x, y)
+  abline(lm(y~x))
+}
+layout(1)
+title(paste("qqplots - Sample 1: normal, Sample 2: exponential. n =", n))
+
+
